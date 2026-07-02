a5cf4229f15089844a03134637c829b544f43777
rainbownash <anastasie.wagenheim@gmail.com>
2026-07-02 11:46:27 +0200

    Create download_models.py

---
+++ scripts/download_models.py
@@ -0,0 +1,34 @@
+import os
+import zipfile
+import gdown
+
+MODEL_URL = "https://drive.google.com/uc?id=1n2bK5y8gB9aX4Ju4H1rdN0uFfynlhSMC"
+
+ZIP_NAME = "models.zip"
+OUTPUT_DIR = "models"
+
+
+def download_models():
+
+    if os.path.exists(OUTPUT_DIR) and any(f.endswith(".pt") for f in os.listdir(OUTPUT_DIR)):
+        print("Los modelos ya están descargados.")
+        return
+
+    print("Descargando modelos...")
+
+    gdown.download(MODEL_URL, ZIP_NAME, quiet=False)
+
+    print("Descomprimiendo modelos...")
+
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    with zipfile.ZipFile(ZIP_NAME, "r") as zip_ref:
+        zip_ref.extractall(OUTPUT_DIR)
+
+    os.remove(ZIP_NAME)
+
+    print(f"Modelos listos en '{OUTPUT_DIR}'")
+
+
+if __name__ == "__main__":
+    download_models()
